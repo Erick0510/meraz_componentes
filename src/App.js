dd7a635334d0a381aa6025407c3bacdeff60bd9b
Erick <erick_3041210064@utd.edu.mx>
2023-01-19 18:17:19 -0600

    V1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+
+const Button = styled.button`
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+// A new component based on Button, but with some override styles
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-color: tomato;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+    <a href="https://utd.edu.mx/" > <TomatoButton>UTD</TomatoButton></a> 
+    <a href='https://github.com/Erick0510'><TomatoButton>GitHub</TomatoButton></a> 
+    <a href='https://www.youtube.com/@instinct5424'><TomatoButton>YouTube</TomatoButton></a> 
+
+  </div>
+         
     </div>
   );
 }
